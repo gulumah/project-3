8edf1fb45658e580807791dea6b5b1614bcb83e0
gulumah <gulumah@itu.edu.tr>
2016-05-23 21:19:05 +0300

    correction

--- topDevelopers.cpp
+++ topDevelopers.cpp
@@ -5,7 +5,7 @@
 #include <iomanip>
 using namespace std;
 int main(){
-	double showlimit,commitNumber, totalCommit=0;
+	double showlimit,commitNumber, totalCommit=0,total;
 	string name,line;
 	ifstream file;  
 	ofstream ofile;
@@ -17,6 +17,7 @@ int main(){
 	}
 	file.close();
 	showlimit = (totalCommit * 80)/100;
+	total=totalCommit;
 	totalCommit=0;
 	file.open("Question2-b/allCommits.txt");
 	ofile.open("Question2-b/80percentage.txt");
@@ -25,7 +26,7 @@ int main(){
 	    in_line >> commitNumber >> name;
 	    totalCommit+=commitNumber;
 	    if(totalCommit <= showlimit)
-	    	ofile << name << " " << commitNumber << " ("<<setprecision(3)<<(commitNumber * 100)/showlimit<<"%)" <<endl;
+	    	ofile << name << " " << commitNumber << " ("<<setprecision(3)<<(commitNumber * 100)/total<<"%)" <<endl;
 	    else
 			break;
 	}
